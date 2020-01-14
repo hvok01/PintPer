b7b88d624436b4574fcb86fa3de08d927a35ec0b
gabriel <aballaygabriel@gmail.com>
2020-01-14 01:11:20 -0300

    commit estructura de carpetas

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+//-----URL
+define("URL","http://localhost/pintper/");
+//-----HOT
+define('HOST','localhost');
+//-----BASE DE DATOS
+define('DB','');
+//-----USUARIO BASE DE DATOS
+define('USER','root');
+//-----PASSWORD BASE DE DATOS
+define('PASS','');
+
+?>
