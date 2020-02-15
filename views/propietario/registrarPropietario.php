8e852d7c33820215cfe35e3eebcd2ed4011a5d84
hvok01 <leonelgomezrodriguez@hotmail.com>
2020-02-15 14:25:16 -0300

    agregado el campo provincia

--- views/propietario/registrarPropietario.php
+++ views/propietario/registrarPropietario.php
@@ -45,6 +45,32 @@
                         <input type="text" name="Correo" placeholder="Correo" class="pintper-textbox"> <br>
 
                         <input type="password" name="Clave" placeholder="Clave" class="pintper-textbox"> <br>
+
+                        <select name="provinicas" class="pintper-option">
+                            <option value="Buenos Aires"></option>
+                            <option value="Catamarca">Catamarca</option>
+                            <option value="Chaco">Chaco</option>
+                            <option value="Chubut">Chubut</option>
+                            <option value="Córdoba">Córdoba</option>
+                            <option value="Corrientes">Corrientes</option>
+                            <option value="Entre Ríos">Entre Ríos</option>
+                            <option value="Formosa">Formosa</option>
+                            <option value="Jujuy">Jujuy</option>
+                            <option value="La Pampa">La Pampa</option>
+                            <option value="La Rioja">La Rioja</option>
+                            <option value="Mendoza">Mendoza</option>
+                            <option value="Misiones">Misiones</option>
+                            <option value="Neuquén">Neuquén</option>
+                            <option value="Río Negro">Río Negro</option>
+                            <option value="Salta">Salta</option>
+                            <option value="San Juan">San Juan</option>
+                            <option value="San Luis" selected>San Luis</option>
+                            <option value="Santa Cruz">Santa Cruz</option>
+                            <option value="Santa Fe">Santa Fe</option>
+                            <option value="Santiago del Estero">Santiago del Estero</option>
+                            <option value="Tierra del Fuego">Tierra del Fuego</option>
+                            <option value="Tucumán">Tucumán</option>
+                        </select> <br>
                         
                         <input type="submit" value="Registrarme" class="pintper-button"> <br>
                     </form>
