8c0c72bc05f8a03a673c1a2f91f776dabb16845d
hvok01 <leonelgomezrodriguez@hotmail.com>
2020-02-15 22:17:29 -0300

    Mapa incorporado y funcionando correctamente

--- views/local/editarMiLocal.php
+++ views/local/editarMiLocal.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8"><link rel="icon" href="<?php echo constant("URL")?>public/img/Favicon2.png">
+    <meta charset="UTF-8">
+    <link rel="icon" href="<?php echo constant("URL")?>public/img/Favicon2.png">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo constant("URL")?>public/css/pintperRoot.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo constant("URL")?>public/css/pintperGrid.css">
     <link rel="stylesheet" type="text/css" href="<?php echo constant("URL")?>public/css/headerPintper.css">
     <link rel="stylesheet" type="text/css" href="<?php echo constant("URL")?>public/css/propietarioStyle.css">
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src='http://maps.google.com/maps/api/js?sensor=false&libraries=places&key=AIzaSyCfIMAGCSKeLJPoUwDPnGz7pB4-exX76Dk'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-locationpicker/0.1.12/locationpicker.jquery.js"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
     <!-- Alpine js -->
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v1.10.1/dist/alpine.js" defer></script>
     <title>Local: editar</title>
@@ -60,12 +68,12 @@
             <div class="pintper-row">
 
                 <div class="pintper-col-8">
-                    <input type="text" name="Nombre" id="" placeholder="Nombre" class="pintper-textbox">
-                    <input type="text" name="Telefono" id="" placeholder="Telefono" class="pintper-textbox"> 
+                    <input type="text" name="Nombre" placeholder="Nombre" class="pintper-textbox">
+                    <input type="text" name="Telefono" placeholder="Telefono" class="pintper-textbox"> 
                 </div>
                 <div class="pintper-col-8">
-                    <input type="text" name="Direccion" id="" placeholder="Direccion" class="pintper-textbox">
-                    <input type="button" value="Seleccionar Ubicacion" class="pintper-button-op2">
+                    <input type="text" name="Direccion" placeholder="Direccion" class="pintper-textbox" id="ModalMapAddress" value="">
+                    <input type="button" value="Seleccionar Ubicacion" class="pintper-button-op2" data-toggle="modal" data-target="#ModalMap">
 
                 </div>
 
@@ -130,6 +138,76 @@
         </form>
         
     </div>
+
+    <!-- implementacion modulo mapa -->
+
+    <div class="modal fade" id="ModalMap" tabindex="-1" role="dialog" >
+        <style>.pac-container { z-index: 99999; }</style>
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-body">
+        <div class="form-horizontal">
+        <div class="form-group">
+        <label class="col-sm-2 control-label">Ubicación:</label>
+        <div class="col-sm-9">
+
+        </div>
+        <div class="col-sm-1">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        </div>
+        </div>
+        <div id="ModalMapPreview" style="width: 100%; height: 400px;"></div>
+        <div class="clearfix">&nbsp;</div>
+        <div class="m-t-small">
+        <label class="p-r-small col-sm-1 control-label">Lat.:</label>
+        <div class="col-sm-3">
+        <input type="text" class="form-control" id="ModalMap-lat" />
+        </div>
+        <label class="p-r-small col-sm-2 control-label">Long.:</label>
+
+        <div class="col-sm-3">
+        <input type="text" class="form-control" id="ModalMap-lon" />
+        </div>
+        <div class="col-sm-3">
+        <button type="button" class="btn btn-primary btn-block" data-dismiss="modal" >Aceptar</button>
+        </div>
+
+        </div>
+        <div class="clearfix"></div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+
+    <script>
+
+        $("#ModalMapPreview").locationpicker({
+
+        location: {
+        latitude: -33.302103,
+        longitude: -66.337091
+        },
+        enableAutocomplete: true,
+        inputBinding: {
+        latitudeInput: $("#ModalMap-lat"),
+        longitudeInput: $("#ModalMap-lon"),
+        locationNameInput: $("#ModalMapAddress")
+        },
+
+
+
+        onchanged: function (currentLocation, radius, isMarkerDropped) {
+        $("#ModalMapAddress").val();
+
+        // console.log($("#ModalMapAddress").val());
+        }
+        });
+        $("#ModalMap").on("shown.bs.modal", function () {
+        $("#ModalMapPreview").locationpicker("autosize");
+        });
+
+    </script>
 
 
 </body>
